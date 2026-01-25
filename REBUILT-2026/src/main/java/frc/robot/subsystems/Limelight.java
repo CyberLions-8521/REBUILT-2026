a5d85a176fafaf0dfd9265056c63b621aa776a43
TacoDuck9800-Stack <lucasvfernandez@gmail.com>
2026-01-24 18:17:15 -0800

    elastic

--- REBUILT-2026/src/main/java/frc/robot/subsystems/Limelight.java
+++ REBUILT-2026/src/main/java/frc/robot/subsystems/Limelight.java
@@ -1,7 +1,7 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.LimelightHelpers;
+import frc.robot.util.LimelightHelpers;
 
 import com.ctre.phoenix6.hardware.CANdle;
 import com.ctre.phoenix6.signals.StripTypeValue;
