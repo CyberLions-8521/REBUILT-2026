cb56c961b07c36a25efe8273e327230251a2fd2f
RubyD <143292762+ohnoruu@users.noreply.github.com>
2026-02-04 14:55:49 -0800

    Remove button mapping for shooter action
    Removed button mapping for shooter action.

--- REBUILT-2026/src/main/java/frc/robot/RobotContainer.java
+++ REBUILT-2026/src/main/java/frc/robot/RobotContainer.java
@@ -14,9 +14,6 @@ public class RobotContainer {
 
   private void configureBindings() {
 
-    // currently making the a button map to fire for 5 seconds
-    m_gamepad.a().onTrue(m_shooter.shoot());
-
   }
 
   public Command getAutonomousCommand() {
