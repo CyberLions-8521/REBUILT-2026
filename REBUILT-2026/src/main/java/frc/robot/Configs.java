a8e24f2c0b4416619cb62d8dfa650f2b15da5dc1
Emma Check <eccheck100@student.hbuhsd.edu>
2026-02-18 17:03:57 -0800

    Motors and Configs

---
+++ REBUILT-2026/src/main/java/frc/robot/Configs.java
@@ -0,0 +1,24 @@
+package frc.robot;
+
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+
+
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
+
+public class Configs {
+
+    public static final class OutputConfigs {
+       public static final TalonFXConfiguration kKrakenLeaderConfig = new TalonFXConfiguration();
+
+       static {
+            kKrakenLeaderConfig.MotorOutput
+                .withNeutralMode(NeutralModeValue.Coast);
+       }
+    }
+
+
+
+
+}
---
+++ REBUILT-2026/src/main/java/frc/robot/subsystems/Output.java
@@ -0,0 +1,70 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.ctre.phoenix6.configs.TalonFXConfigurator;
+import com.ctre.phoenix6.controls.Follower;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.MotorAlignmentValue;
+import frc.robot.Configs.OutputConfigs;
+
+public class Output extends SubsystemBase {
+  /** Creates a new ExampleSubsystem. */
+  
+  private TalonFX m_motorBotLeader;
+  private Follower m_motorBotFollower;
+
+  private TalonFX m_motorTopLeader;
+  private Follower m_motorTopFollower;
+
+  private OutputConfigs m_configs = new OutputConfigs();
+  
+  public Output(int motorBotLeadID, int motorBotFolID, int motorTopLeadID, int motorTopFolID) {
+    m_motorBotLeader = new TalonFX(motorBotLeadID);
+    m_motorBotFollower = new Follower(motorBotFolID, MotorAlignmentValue.Opposed);
+    m_motorTopLeader = new TalonFX(motorTopLeadID);
+    m_motorTopFollower = new Follower(motorTopFolID, MotorAlignmentValue.Opposed);
+
+    m_motorBotLeader.getConfigurator().apply(m_configs.kKrakenLeaderConfig);
+    m_motorTopLeader.getConfigurator().apply(m_configs.kKrakenLeaderConfig);
+  }
+
+  /**
+   * Example command factory method.
+   *
+   * @return a command
+   */
+  public Command exampleMethodCommand() {
+    // Inline construction of command goes here.
+    // Subsystem::RunOnce implicitly requires `this` subsystem.
+    return runOnce(
+        () -> {
+          /* one-time action goes here */
+        });
+  }
+
+  /**
+   * An example method querying a boolean state of the subsystem (for example, a digital sensor).
+   *
+   * @return value of some boolean subsystem state, such as a digital sensor.
+   */
+  public boolean exampleCondition() {
+    // Query some boolean state, such as a digital sensor.
+    return false;
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
