311fd28d957e57339b4edb8e9ee90757d3a1f14c
Pnbcris <cristian64470@gmail.com>
2026-02-21 12:10:39 -0800

    Referenced constants from other teams code; begun creating skeleton of IntakeVision class

--- REBUILT-2026/src/main/java/Constants.java
+++ REBUILT-2026/src/main/java/Constants.java
@@ -6,20 +6,24 @@ import edu.wpi.first.math.util.Units;
 public class Constants {
     
 public static final class VisionConstants {
-    public static final double maximumAmbiguity = 0.2; // adjust after testing
+    public static final double MIN_TARGET_AREA = 0.1; // minimum area of target to be considered valid; change after testing 
+    public static final double TARGET_EXP_TIME = 1.0; //expire time for target when lost; change after testing
 
-    public static double intakeCameraXOffset = 12.0; // x offset from center to intake camera (inches); change when positioned
-    public static double intakeCameraYOffset = 10.0; // y offset from center to intake camera (inches); change when positioned
-    public static double intakeCameraZOffset = 12.0; // z offset from ground to camera (inches); change when positioned
+    public static double intakeCam_XOffset = 0.0; // x offset from center to intake camera (inches); change when positioned
+    public static double intakeCam_YOffset = 0.0; // y offset from center to intake camera (inches); change when positioned
+    public static double intakeCam_ZOffset = 0.0; // z offset from ground to camera (inches); change when positioned
     
-    public static double intakeCameraRoll = 0.0; // roll of intake camera in degrees; change when positioned
-    public static double intakeCameraPitch = 0.0; // pitch of intake camera in degrees; change when positioned
-    public static double intakeCameraYaw = 0.0; // yaw of intake camera in degrees; change when positioned
+    public static double intakeCam_Roll = 0.0; // roll of intake camera in degrees; change when positioned
+    public static double intakeCam_Pitch = 0.0; // pitch of intake camera in degrees; change when positioned
+    public static double intakeCam_Yaw = 0.0; // yaw of intake camera in degrees; change when positioned
 
-    public static final Transform3d INTAKE_CAMERA_TRANSFORM = new Transform3d(
-        new Translation3d(Units.inchesToMeters(intakeCameraXOffset), Units.inchesToMeters(intakeCameraYOffset), Units.inchesToMeters(intakeCameraZOffset)),
-        new Rotation3d(Units.degreesToRadians(intakeCameraRoll), Units.degreesToRadians(intakeCameraPitch), Units.degreesToRadians(intakeCameraYaw))
-    ); // transform 3d position for camera given in meters; remove conversion if using inches 
+    public static final Transform3d INTAKE_CAMERA_POSITION = new Transform3d(
+        new Translation3d(Units.inchesToMeters(intakeCam_XOffset), Units.inchesToMeters(intakeCam_YOffset), Units.inchesToMeters(intakeCam_ZOffset)),
+        new Rotation3d(Units.degreesToRadians(intakeCam_Roll), Units.degreesToRadians(intakeCam_Pitch), Units.degreesToRadians(intakeCam_Yaw))); 
+        // transform 3d position for camera given in meters; remove conversion if using inches 
+
+    public static final Transform3d CAMERA_TO_FRONT = INTAKE_CAMERA_POSITION.inverse(); 
+
 }
 
 public static final class CameraConstants{
---
+++ REBUILT-2026/src/main/java/frc/robot/Subsystems/IntakeVision.java
@@ -0,0 +1,35 @@
+package frc.robot.Subsystems;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+// import frc.robot.Constants.CameraConstants; // will uncomment once logic is figured out
+// import frc.robot.Constants.VisionConstants; // will uncomment once logic is figured out
+
+public class IntakeVision extends SubsystemBase {
+// will uncomment and implement once logic is figured out
+    /* 
+    public IntakeVision() {
+        // constructor for intake vision subsystem; initialize cameras and other components here
+    }
+
+    public boolean hasTarget() {
+        // return true if target is detected by vision system; implement logic to determine if target is valid based on area and other factors
+        return false;
+    }
+    
+    public double getYaw() {
+        return 0.0; // default value
+    }
+    
+    public double getDistance() {
+        return 0.0; // default value
+    }
+
+    public double getArea() {
+        return 0.0; // default value
+    }
+
+    public boolean targetExpired() {
+        // return true if target has been lost for longer than the expire time; implement logic to track when target was last seen and compare to current time
+        return false;
+    }
+    */
+}
