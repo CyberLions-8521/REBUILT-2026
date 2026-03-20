a869c0c9e6482c589f1089ee1547f32a104810d9
Kevin On <kon100@student.hbuhsd.edu>
2026-03-20 15:00:45 -0700

    Added CheckLimelight Method that is able to turn to a specfic color depending on itf its inrange, sees the apirltag, does not see april tag, and is centered with the apirl tag

--- REBUILT-2026/src/main/java/frc/robot/subsystems/LEDLights.java
+++ REBUILT-2026/src/main/java/frc/robot/subsystems/LEDLights.java
@@ -1,45 +1,49 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems;
 
+/* 
+import com.ctre.phoenix6.controls.StrobeAnimation;
+import com.ctre.phoenix6.controls.TwinkleAnimation;
 import com.ctre.phoenix6.CANBus;
 import com.ctre.phoenix6.controls.ColorFlowAnimation;
-import com.ctre.phoenix6.controls.ControlRequest;
-import com.ctre.phoenix6.controls.EmptyAnimation;
 import com.ctre.phoenix6.controls.FireAnimation;
 import com.ctre.phoenix6.controls.RgbFadeAnimation;
 import com.ctre.phoenix6.controls.SingleFadeAnimation;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+*/
+
+import com.ctre.phoenix6.controls.ControlRequest;
+import com.ctre.phoenix6.controls.EmptyAnimation;
 import com.ctre.phoenix6.controls.SolidColor;
-import com.ctre.phoenix6.controls.StrobeAnimation;
-import com.ctre.phoenix6.controls.TwinkleAnimation;
 import com.ctre.phoenix6.hardware.CANdle;
 import com.ctre.phoenix6.signals.RGBWColor;
 
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.units.measure.Force;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.LimelightHelpers;
 import frc.robot.utils.Configs.CANdleConfigs;
 import frc.robot.utils.Constants.CANdleConstants;
-import frc.robot.utils.Constants.LimitSwitchConstants;
+import frc.robot.utils.Constants.LimelightConstants;
+import frc.robot.subsystems.Shooter;
 
 public class LEDLights extends SubsystemBase {
+  private final CANdle m_CANdle = new CANdle(CANdleConstants.CANdleID, CANdleConstants.kCanbusName);
+  private static int m_LEDcount = 256;
 
-  DigitalInput m_leftLimitSwitch = new DigitalInput(LimitSwitchConstants.kLeftLimitSwitchID);
-  DigitalInput m_rightLimitSwith = new DigitalInput(LimitSwitchConstants.kRightLimitSwitchID);
+  public boolean seeAprilTag;
+  public boolean centeredAprilTag;
+  public boolean inRange;
 
   public enum LEDMode {
     Off (new EmptyAnimation(0)),
-    SeesApriltag (new StrobeAnimation(0, CANdleConstants.ledCount - 1).withColor(new RGBWColor(0, 255, 0))), //limelight can see the apriltag
-    TargetingApriltag (new SolidColor(0, CANdleConstants.ledCount -1).withColor(new RGBWColor(0, 255, 0))), //limelight is targeting the tag
-    Shooting (new FireAnimation(0, CANdleConstants.ledCount - 1).withBrightness(1.0)),
-    Charging (new StrobeAnimation(0, CANdleConstants.ledCount - 1).withColor(new RGBWColor(255, 35, 0))),
-    Intaking (new TwinkleAnimation(0, CANdleConstants.ledCount - 1).withColor(new RGBWColor(255,115,0))),
-    LimitSwitchDetected (new SolidColor(0, CANdleConstants.ledCount -1).withColor(new RGBWColor(0,255,0)));
-    
+    SeesAprilTag (new SolidColor(0, m_LEDcount - 1).withColor( new RGBWColor(255,150,0))),
+    OnAprilTag (new SolidColor(0, m_LEDcount - 1).withColor( new RGBWColor(255,255,0))),
+    InRange(new SolidColor(0, m_LEDcount - 1).withColor( new RGBWColor(0, 0, 255))),
+    AllChecks(new SolidColor(0, m_LEDcount - 1).withColor( new RGBWColor(0, 255, 0)));
+
     public final ControlRequest animation;
 
     private LEDMode (ControlRequest animation) {
@@ -47,40 +51,63 @@ public class LEDLights extends SubsystemBase {
     }
   }
 
-  private final CANdle m_CANdle = new CANdle(CANdleConstants.CANdleID, CANdleConstants.kCanbusName);
-
-
   public LEDLights() {
     m_CANdle.getConfigurator().apply(CANdleConfigs.CANdleConfig);
   }
 
-  public Command setLEDCommand(LEDMode newMode) {
-    return new RunCommand(() -> m_CANdle.setControl(newMode.animation), this);
+
+  public void setLEDCommand( LEDMode newMode) {
+    m_CANdle.setControl(newMode.animation);
   }
 
-  public boolean isLimitSwitchPressed(){
-    return !m_leftLimitSwitch.get() && !m_rightLimitSwith.get();
+  // public Command setLEDCommandTimed(LEDMode newMode, double time) {
+  //   return new RunCommand(() -> m_CANdle.setControl(newMode.animation), this)
+  //                         .withTimeout(time)
+  //                         .andThen(setLEDCommand(LEDMode.Off));
+  // } 
+
+  public void ForceOff() {
+    m_CANdle.setControl(LEDMode.Off.animation);
   }
 
-  public Command limitSwitchLEDCommand() {
-    return new RunCommand(() -> {
-      m_CANdle.setControl(isLimitSwitchPressed() ? LEDMode.LimitSwitchDetected.animation : LEDMode.Off.animation);
-    }, this);
+  public Command CheckLimelight() {
+    return new RunCommand(
+      () -> 
+      {
+      seeAprilTag = LimelightHelpers.getTV(LimelightConstants.limelightName);
+      centeredAprilTag = Math.abs(LimelightHelpers.getTX(LimelightConstants.limelightName)) < 1;
+      inRange = Shooter.getDistance() > 1.5;
+      
+      if (seeAprilTag){
+        setLEDCommand(LEDMode.SeesAprilTag);
+        if(!(inRange && centeredAprilTag)){
+          if(inRange){
+            setLEDCommand(LEDMode.InRange);
+          }
+          if (centeredAprilTag) 
+          { 
+            setLEDCommand(LEDMode.OnAprilTag);
+          }
+        } else {
+          setLEDCommand(LEDMode.AllChecks);
+        }
+      }
+      else { 
+        ForceOff(); 
+      }
+      },this);
   }
-
- 
+  /* Sample Codew
+   * if (LimelightHelpers.getTVA() && keycode.x is pressed down) {
+   *  activate the red one
+   * } 
+   * if (LimelightHelpers.getTVA() && keycode.x is pressed down && its within tolerance) }
+   *  activate the green one
+   * }
+   * activate off;
+   */
   @Override
   public void periodic() {
-    SmartDashboard.putBoolean("LimitSwitchStatus", !m_leftLimitSwitch.get());
-
-    if(!m_leftLimitSwitch.get() && !m_rightLimitSwith.get()){
-      m_CANdle.setControl(LEDMode.Shooting.animation);
-    } 
-    else {
-       m_CANdle.setControl(LEDMode.Off.animation);
-    }
-    
-
-
+  
   }
 }
